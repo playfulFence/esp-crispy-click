d55e61ecb9d41503f338a49c750d5f94459fb9b1
playfulFence <konnor1980@yandex.ru>
2022-09-05 19:22:20 +0200

    update code for Wokwi simulation

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
 #![no_std]
 #![no_main]
 
-//use std::thread;
-
-#[cfg(feature="esp32")]
-use esp32_hal as hal;
-#[cfg(feature="esp32s2")]
-use esp32s2_hal as hal;
-#[cfg(feature="esp32s3")]
-use esp32s3_hal as hal;
-#[cfg(feature="esp32c3")]
-use esp32c3_hal as hal;
-
-use hal::{
+use esp32s2_hal::{
     clock::ClockControl,
     pac::Peripherals,
     gpio_types::*,
@@ -26,13 +15,10 @@ use hal::{
 };
 
 /* Display and graphics */
-#[cfg(feature = "ili9341")]
 use ili9341::{DisplaySize240x320, Ili9341, Orientation};
-#[cfg(feature = "st7789")]
-use st7789::*;
 
-/* Display and graphics stuff */
 use display_interface_spi::SPIInterfaceNoCS;
+
 use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::pixelcolor::*;
 use embedded_graphics::prelude::*;
@@ -43,18 +29,20 @@ use embedded_graphics::geometry::*;
 use embedded_graphics::draw_target::DrawTarget;
 use embedded_hal;
 
-/* Fonts */
-use profont::{PROFONT_24_POINT, PROFONT_18_POINT};
 
 
 
-#[cfg(feature="xtensa-lx-rt")]
+use profont::{PROFONT_14_POINT, PROFONT_18_POINT};
+
 use xtensa_lx_rt::entry;
-#[cfg(feature="riscv-rt")]
-use riscv_rt::entry;
 
 use esp_println::println;
 use esp_backtrace as _;
+
+const textStyle : TextStyle = TextStyleBuilder::new()
+    .alignment(embedded_graphics::text::Alignment::Center)
+    .baseline(embedded_graphics::text::Baseline::Middle)
+    .build();
 
 
 /* Some stuff for correct orientation and color on ILI9341 */
@@ -73,7 +61,7 @@ impl ili9341::Mode for KalugaOrientation {
             Self::LandscapeVericallyFlipped => 0x20,
             Self::Landscape => 0x20 | 0x40,
             Self::PortraitFlipped => 0x80 | 0x40,
-            Self::LandscapeFlipped => 0x80 | 0x20,
+            Self::LandscapeFlipped => 0x80 | 0x20 | 0x08,
         }
     }
 
@@ -82,7 +70,6 @@ impl ili9341::Mode for KalugaOrientation {
     }
 }
 
-/* Debouncing algorythm */
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum Event {
@@ -141,13 +128,11 @@ impl<T: ::embedded_hal::digital::v2::InputPin<Error = core::convert::Infallible>
 
  
 
+
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
 
-    #[cfg(any(feature = "esp32"))]
-    let mut system = peripherals.DPORT.split();
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3", feature = "esp32c3"))]
     let mut system = peripherals.SYSTEM.split();
 
     let mut clocks = ClockControl::boot_defaults(system.clock_control).freeze();
@@ -169,93 +154,27 @@ fn main() -> ! {
     
 
     /* Set corresponding pins */
-    #[cfg(feature = "esp32")]
-    let mosi = io.pins.gpio23;
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let mosi = io.pins.gpio7;
-    #[cfg(feature = "esp32c3")]
-    let mosi = io.pins.gpio7;
+ 
+    let mosi = io.pins.gpio35;
+    let cs = io.pins.gpio15;
+    let rst = io.pins.gpio4;
+    let dc = io.pins.gpio2;
+    let sck = io.pins.gpio36;
+    let miso = io.pins.gpio10;
+    let backlight = io.pins.gpio6;
 
-    #[cfg(feature = "esp32")]
-    let cs = io.pins.gpio22;
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let cs = io.pins.gpio5;
-    #[cfg(feature = "esp32c3")]
-    let cs = io.pins.gpio20;
-
-    #[cfg(feature = "esp32")]
-    let rst = io.pins.gpio18;
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let rst = io.pins.gpio18;
-    #[cfg(feature = "esp32c3")]
-    let rst = io.pins.gpio3;
-
-    #[cfg(feature = "esp32")]
-    let dc = io.pins.gpio21;
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let dc = io.pins.gpio4;
-    #[cfg(feature = "esp32c3")] 
-    let dc = io.pins.gpio21;
-
-    #[cfg(feature = "esp32")]
-    let sck = io.pins.gpio19;
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let sck = io.pins.gpio6;
-    #[cfg(feature = "esp32c3")]
-    let sck = io.pins.gpio6;
-
-    #[cfg(feature = "esp32")]
-    let miso = io.pins.gpio25;
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let miso = io.pins.gpio12;
-    #[cfg(feature = "esp32c3")]
-    let miso = io.pins.gpio8;
-
-    #[cfg(feature = "esp32")]
-    let mut backlight = io.pins.gpio5.into_push_pull_output();
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
-    let mut backlight = io.pins.gpio9.into_push_pull_output();
-    #[cfg(feature = "esp32c3")]
-    let mut backlight = io.pins.gpio0.into_push_pull_output();
-
+    
     /* Then set backlight (set_low() - display lights up when signal is in 0, set_high() - opposite case(for example.)) */
     let mut backlight = backlight.into_push_pull_output();
     backlight.set_high().unwrap();
 
-
-    /* Configure SPI */
-    #[cfg(feature = "esp32")]
-    let spi = spi::Spi::new(
-        peripherals.SPI3,
-        sck,
-        mosi,
-        miso,
-        cs,
-        100u32.MHz(),
-        spi::SpiMode::Mode0,
-        &mut system.peripheral_clock_control,
-        &mut clocks,
-    );
-    #[cfg(any(feature = "esp32s2", feature = "esp32s3"))]
     let spi = spi::Spi::new(
         peripherals.SPI2,
         sck,
         mosi,
         miso,
         cs,
-        80u32.MHz(),
-        spi::SpiMode::Mode0,
-        &mut system.peripheral_clock_control,
-        &mut clocks,
-    );
-    #[cfg(feature = "esp32c3")]
-    let spi = spi::Spi::new(
-        peripherals.SPI2,
-        sck,
-        mosi,
-        miso,
-        cs,
-        80u32.MHz(),
+        800u32.MHz(),
         spi::SpiMode::Mode0,
         &mut system.peripheral_clock_control,
         &mut clocks,
@@ -264,77 +183,134 @@ fn main() -> ! {
     let di = SPIInterfaceNoCS::new(spi, dc.into_push_pull_output());
     let reset = rst.into_push_pull_output();
     let mut delay = Delay::new(&clocks);
-    #[cfg(feature = "ili9341")]
-    let mut display = Ili9341::new(di, reset, &mut delay, KalugaOrientation::Landscape, DisplaySize240x320).unwrap();
-    #[cfg(feature = "st7789")]
-    let mut display = st7789::ST7789::new(di, reset, 240, 240);
 
-    /* st7789 display requires additional init functions */
 
-    #[cfg(feature = "st7789")]
-    display.init(&mut delay).unwrap();
-    #[cfg(feature = "st7789")]
-    display.set_orientation(st7789::Orientation::Portrait).unwrap();
+    let mut display = Ili9341::new(di, reset, &mut delay, KalugaOrientation::LandscapeFlipped, DisplaySize240x320).unwrap();
     
+    println!("Display initialized");
 
-    println!("Initialized");
+    display.clear(Rgb565::WHITE).unwrap(); 
 
-    display.clear(Rgb565::WHITE).unwrap();
+    let mut button_green = Button::new(io.pins.gpio3.into_pull_up_input());
+    let mut button_blue  = Button::new(io.pins.gpio13.into_pull_up_input());
 
-    /* Initialise buttons */
 
-    /* All available buttons for ESP32-S3-USB-OTG */
-    let mut button0 = Button::new(io.pins.gpio10.into_pull_up_input()); //io.pins.gpio10.into_pull_up_input(); // UP button on esp32-s3-usb-otg (low level when pressed)
-    let mut button1 = Button::new(io.pins.gpio11.into_pull_up_input()); // DOWN button on esp32-s3-usb-otg (low level when pressed)
-    let mut button2 = Button::new(io.pins.gpio14.into_pull_up_input()); // MENU button on esp32-s3-usb-otg (low level when pressed)
+ 
+    Text::with_text_style("Press GREEN button",
+            display.bounding_box().center() - Size::new(0, 35), 
+            MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::BLACK),
+            textStyle,
+    )
+    .draw(&mut display)
+    .unwrap();
 
-    
-    /* Counters for number of pushes of each button.
-       Just to verify, that debouncing works correctly  */
-    let mut menu_cnt = 0;
-    let mut down_cnt = 0; 
-    let mut up_cnt   = 0;
 
+    Text::with_text_style("Press BLUE button",
+            display.bounding_box().center() + Size::new(0, 25), 
+            MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::BLACK),
+            textStyle,
+    )
+    .draw(&mut display)
+    .unwrap();
+
+    let mut green_cnt = 0;
+    let mut blue_cnt = 0;
+
+    let mut last_pressed_blue : bool = false;
 
     loop {
-
-        if let Event::Pressed = button2.poll()
+        if let Event::Pressed = button_green.poll()
         {
-            menu_cnt += 1;
+            green_cnt += 1;
 
-            display.clear(Rgb565::WHITE).unwrap();
-            println!("Menu! (x{})", menu_cnt);
-            Text::new("Menu!",
-                    display.bounding_box().center() - Size::new(display.bounding_box().size.width/2 - 10, 0), 
-                    MonoTextStyle::new(&PROFONT_24_POINT, Rgb565::BLACK))
-            .draw(&mut display)
-            .unwrap();
-        }
-        if let Event::Pressed = button0.poll()
-        {
-            up_cnt += 1;
-
-            display.clear(Rgb565::WHITE).unwrap();
-            println!("Up! (x{})", up_cnt);
-            Text::new("Up!",
-                    display.bounding_box().center() - Size::new(display.bounding_box().size.width/2 - 10, 0), 
-                    MonoTextStyle::new(&PROFONT_24_POINT, Rgb565::BLACK))
-            .draw(&mut display)
-            .unwrap();
-        }
-        if let Event::Pressed = button1.poll()
-        {
-            down_cnt += 1;
-
-            display.clear(Rgb565::WHITE).unwrap();
-            println!("Down! (x{})", down_cnt);
-            Text::new("Down!",
-                    display.bounding_box().center() - Size::new(display.bounding_box().size.width/2 - 10, 0), 
-                    MonoTextStyle::new(&PROFONT_24_POINT, Rgb565::BLACK))
-            .draw(&mut display)
-            .unwrap();
-        }
             
+            Rectangle::with_center(display.bounding_box().center() - Size::new(0, 30), Size::new(270, 30))
+            .into_styled(
+                PrimitiveStyleBuilder::new()
+                    .fill_color(Rgb565::WHITE)
+                    .stroke_color(Rgb565::WHITE)
+                    .stroke_width(1)
+                    .build(),
+            )
+            .draw(&mut display);
 
+                Text::with_text_style("Green button pressed!",
+                    display.bounding_box().center() - Size::new(0, 35), 
+                    MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::CSS_GREEN),
+                    textStyle,
+                )
+                .draw(&mut display)
+                .unwrap();
+
+            println!("Green! (x{})", green_cnt);
+
+            if last_pressed_blue {
+                Rectangle::with_center(display.bounding_box().center() + Size::new(0, 30), Size::new(270, 30))
+                    .into_styled(
+                        PrimitiveStyleBuilder::new()
+                            .fill_color(Rgb565::WHITE)
+                            .stroke_color(Rgb565::WHITE)
+                            .stroke_width(1)
+                            .build(),
+                    )
+                    .draw(&mut display);
+                
+
+                Text::with_text_style("Press BLUE button",
+                    display.bounding_box().center() + Size::new(0, 25), 
+                    MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::BLACK),
+                    textStyle,
+                )
+                .draw(&mut display)
+                .unwrap();
+            }
+            last_pressed_blue = false;
+        }
+        if let Event::Pressed = button_blue.poll()
+        {
+            blue_cnt += 1;
+
+                Rectangle::with_center(display.bounding_box().center() + Size::new(0, 30), Size::new(270, 30))
+                .into_styled(
+                    PrimitiveStyleBuilder::new()
+                        .fill_color(Rgb565::WHITE)
+                        .stroke_color(Rgb565::WHITE)
+                        .stroke_width(1)
+                        .build(),
+                )
+                .draw(&mut display);
+            
+                Text::with_text_style("Blue button pressed!",
+                            display.bounding_box().center() + Size::new(0, 25), 
+                            MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::BLUE),
+                            textStyle,
+                )
+                .draw(&mut display)
+                .unwrap();
+            
+            println!("Blue! (x{})", blue_cnt);
+
+            if !last_pressed_blue {
+
+                Rectangle::with_center(display.bounding_box().center() - Size::new(0, 30), Size::new(270, 30))
+                .into_styled(
+                    PrimitiveStyleBuilder::new()
+                        .fill_color(Rgb565::WHITE)
+                        .stroke_color(Rgb565::WHITE)
+                        .stroke_width(1)
+                        .build(),
+                )
+                .draw(&mut display);
+
+                Text::with_text_style("Press GREEN button",
+                    display.bounding_box().center() - Size::new(0, 35), 
+                    MonoTextStyle::new(&PROFONT_18_POINT, Rgb565::BLACK),
+                    textStyle,
+                )
+                .draw(&mut display)
+                .unwrap();
+            }
+            last_pressed_blue = true;
+        }
     }
 }
